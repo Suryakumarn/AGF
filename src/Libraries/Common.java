ae65810238e309687797733d79adf9f5a7cd85b1
Suryakumarn <suryakumar9292@yahoo.com>
2017-08-14 17:16:05 +0530

    Signed-off-by: Suryakumarn <suryakumar9292@yahoo.com>

--- src/Libraries/Common.java
+++ src/Libraries/Common.java
@@ -1,5 +1,10 @@
 package Libraries;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Common extends Driver 
 {
@@ -8,8 +13,11 @@ public class Common extends Driver
 		try
 		{
 			
-			Browser.OpenBrowser("chrome", "https://www.google.co.in/");
-			Result.takescreenshot("check");
+			/*Browser.OpenBrowser("chrome", "https://www.google.co.in/");
+			Result.takescreenshot("check");*/
+			setUp();
+			testGooglePageTitleInIEBrowser();
+			tearDown();
 		}
 		catch(Exception e)
 		{
@@ -17,6 +25,41 @@ public class Common extends Driver
 		}
 		
 	}	
+	
+//	static String driverPath = "D:\\Chrome_Drivers\\chromedriver2.31\\";
+	public static RemoteWebDriver driver;
+	
+	public static void setUp() {
+		System.out.println("launching Chrome browser");
+		System.setProperty("webdriver.chrome.driver", Driver.basepth.get()+"\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	public static void testGooglePageTitleInIEBrowser() throws InterruptedException {
+		
+		driver.navigate().to("http://chatbotui.eu-gb.mybluemix.net/");
+		driver.get("http://chatbotui.eu-gb.mybluemix.net/");
+		Thread.sleep(1000);
+		WebDriverWait wait = new WebDriverWait(driver, 60);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@target='_blank' and text()='Tour of Heroes']")));
+		if((driver.findElement(By.xpath("//a[@target='_blank' and text()='Tour of Heroes']")).isDisplayed()) && 
+				(driver.findElement(By.xpath("//a[@target='_blank' and text()='CLI Documentation']")).isDisplayed()) && 
+				(driver.findElement(By.xpath("//a[@target='_blank' and text()='Angular blog']")).isDisplayed())){
+			System.out.println("The Links are Displayed");
+			Result.takescreenshot("check");
+		}else{
+			System.out.println("The Links are not Displayed");
+			Result.takescreenshot("check");
+		}
+	}
+
+	public static void tearDown() {
+		if(driver!=null) {
+			System.out.println("Closing Chrome browser");
+			driver.quit();
+		}
+	}
 	
 	
 }
